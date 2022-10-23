289eecbea59f216dc97528ceef722d2bc4a68b21
ujihisa <ujihisa@users.noreply.github.com>
2022-10-23 11:19:47 -0400

    Add feature image support

--- image.sh
+++ image.sh
@@ -6,8 +6,15 @@ if [ -z $DATE ]; then
 fi
 
 for FNAME in $(ls -c /tmp/*.{jpg,jpeg,png} | tac); do
-  NEW_FILENAME=`echo "${FNAME}" | sed -e "s|/tmp/|assets/images/${DATE}-|"`
-  echo $NEW_FILENAME
-  magick convert -scale '1600x1200>' "${FNAME}" "${NEW_FILENAME}"
-  magick mogrify -resize '1600:1200^' "${NEW_FILENAME}"
+  if [[ "${FNAME}" =~ /feature\. ]]; then
+    NEW_FILENAME=`echo "${FNAME}" | sed -e "s|/tmp/|assets/images/${DATE}-|"`
+    echo $NEW_FILENAME
+    magick convert -scale '1200x900' "${FNAME}" "${NEW_FILENAME}"
+    magick convert -gravity center -crop '1200x628+0+0' "${NEW_FILENAME}" "${NEW_FILENAME}"
+  else
+    NEW_FILENAME=`echo "${FNAME}" | sed -e "s|/tmp/|assets/images/${DATE}-|"`
+    echo $NEW_FILENAME
+    magick convert -scale '1600x1200>' "${FNAME}" "${NEW_FILENAME}"
+    # magick mogrify -resize '1600:1200^' "${NEW_FILENAME}"
+  fi
 done
