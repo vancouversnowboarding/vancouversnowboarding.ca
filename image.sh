79c391e4789525e96859f02d213ea07352f18c49
ujihisa <ujihisa@users.noreply.github.com>
2024-11-26 14:31:36 -0800

    Fix `WARNING: The convert command is deprecated in IMv7, use "magick" instead of "convert" or "magick convert"`

--- image.sh
+++ image.sh
@@ -9,12 +9,12 @@ for FNAME in $(ls -c /tmp/*.{jpg,jpeg,JPG,png,PNG} | tac); do
   if [[ "${FNAME}" =~ /feature\. ]]; then
     NEW_FILENAME=`echo "${FNAME}" | sed -e "s|/tmp/|assets/images/${DATE}-|"`
     echo $NEW_FILENAME
-    magick convert -scale '1200x900' "${FNAME}" "${NEW_FILENAME}"
-    magick convert -gravity center -crop '1200x628+0+0' "${NEW_FILENAME}" "${NEW_FILENAME}"
+    magick "${FNAME}" -scale '1200x900' "${NEW_FILENAME}"
+    magick "${NEW_FILENAME}" -gravity center -crop '1200x628+0+0' "${NEW_FILENAME}"
   else
     NEW_FILENAME=`echo "${FNAME}" | sed -e "s|/tmp/|assets/images/${DATE}-|"`
     echo $NEW_FILENAME
-    magick convert -scale '1200x900>' "${FNAME}" "${NEW_FILENAME}"
+    magick "${FNAME}" -scale '1200x900>' "${NEW_FILENAME}"
     # magick mogrify -resize '1600:1200^' "${NEW_FILENAME}"
   fi
 done
